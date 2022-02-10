065ea5d3eef56015d19f2fbd40bd6ca501654b59
natalie <natalie-hasselmann@web.de>
2022-02-10 14:03:29 +0100

    init

---
+++ main.py
@@ -0,0 +1,156 @@
+import sqlite3
+import math
+import random
+import matplotlib.pyplot as plt
+
+
+####### todos
+# verkauf2019 up 0,2x
+# verkauf2020 down -0,01x
+# verkauf2021 up 0,1x
+# alles in einer grapik, jeweils eigene tabelle mit standardabweichung und mittelwert
+# große abweichungen von der standardabweichung herausnehmen
+# in peaks reinzoomen
+# trends erkennen
+#######
+
+# connect to a sqlite database
+def database_connection():
+    try:
+        sqliteConnection = sqlite3.connect('/Users/natalie/Documents/Data Mining/Verkauf.db')
+        cursor = sqliteConnection.cursor()
+        print("Connected to Verkauf database")
+    # error handling
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+
+    return cursor, sqliteConnection
+
+
+# get table name from console
+def get_table_name():
+    print("Enter table name: ")
+    return str(input())
+
+
+# generate sales data
+def generate_data(cursor, sqliteConnection):
+    print("Generate data for")
+    table_name = get_table_name()
+
+    for i in range(1, 365):
+        # valentines day
+        if 38 <= i <= 42:
+            rand_num = random.randint(100, 120)
+            sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 200
+        # summer sale
+        elif 182 <= i <= 235:
+            rand_num = random.randint(80, 85)
+            sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50
+        # Black Friday
+        elif 300 <= i <= 307:
+            rand_num = random.randint(100, 120)
+            sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50
+        # christmas
+        elif 335 <= i <= 356:
+            rand_num = random.randint(80, 90)
+            sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50
+        # end of the year
+        elif 357 <= i <= 364:
+            rand_num = round(random.uniform(0, 5), 2)
+            sinus_result = 0.1 * i + rand_num * math.sin((i + 80) * 16 * math.pi / 365) + 50
+        else:
+            sinus_result = 0.1 * i + 50 * math.sin((i + 80) * 16 * math.pi / 365) + 50
+
+        # Insert data into database
+        try:
+            sqlite_insert_query = f'Insert Into {table_name} (day, sales) Values ({i}, {round(sinus_result, 2)});'
+            cursor.execute(sqlite_insert_query)
+            sqliteConnection.commit()
+        except sqlite3.Error as error:
+            print("Error while inserting data: ", error)
+
+    print("finished generating data")
+
+
+# fetch data
+def fetch_data(cursor, sqliteConnection):
+    print("fetch data from...")
+    table_name = get_table_name()
+    sqlite_select_query = f'select * From {table_name};'
+    cursor.execute(sqlite_select_query)
+    record = cursor.fetchall()
+    return record
+
+
+# close all connections
+def close_connection(cursor, sqliteConnection):
+    if sqliteConnection:
+        cursor.close()
+        sqliteConnection.close()
+        print("The SQLite connection is closed")
+
+
+# plot data from a single table
+def plot_single_table(cursor, sqliteConnection):
+    print("Select a table to plot")
+    table_name = get_table_name()
+    sqlite_select_query = f'Select day From {table_name};'
+    cursor.execute(sqlite_select_query)
+    recordX = cursor.fetchall()
+    sqlite_select_query = f'Select sales From {table_name};'
+    cursor.execute(sqlite_select_query)
+    recordY = cursor.fetchall()
+
+    plt.plot(recordX, recordY)
+    plt.show()
+
+
+# plot all 3 tables
+def plot_all_tables(cursor, sqliteConnection):
+    print("Plot data from sales 2019-2021...")
+
+    select_query = 'Select day from verkauf2019;'
+    cursor.execute(select_query)
+    sales19_x = cursor.fetchall()
+    select_query = 'Select sales from verkauf2019;'
+    cursor.execute(select_query)
+    sales19_y = cursor.fetchall()
+
+    select_query = 'Select day from verkauf2020;'
+    cursor.execute(select_query)
+    sales20_x = cursor.fetchall()
+    select_query = 'Select sales from verkauf2020;'
+    cursor.execute(select_query)
+    sales20_y = cursor.fetchall()
+
+    select_query = 'Select day from verkauf2021;'
+    cursor.execute(select_query)
+    sales21_x = cursor.fetchall()
+    select_query = 'Select sales from verkauf2021;'
+    cursor.execute(select_query)
+    sales21_y = cursor.fetchall()
+
+    plt.plot(sales19_x, sales19_y, label='sales2019')
+    plt.plot(sales20_x, sales20_y, label='sales2020')
+    plt.plot(sales21_x, sales21_y, label='sales2021')
+    plt.xlabel('day')
+    plt.ylabel('sales')
+    plt.title('Sales from 2019 - 2021')
+    plt.legend()
+    plt.show()
+
+
+# get mean from sales table
+# def get_mean(sqliteConnection):
+#     data = pd.read_sql("Select * From verkauf2020", con=sqliteConnection)
+#     mean = data["sales"].mean()
+#     print("mean: ", mean)
+
+
+cursor, sqliteConnection = database_connection()
+# generate_data(cursor, sqliteConnection)
+# record = fetch_data(cursor, sqliteConnection)
+# plot_single_table(cursor, sqliteConnection)
+plot_all_tables(cursor, sqliteConnection)
+close_connection(cursor, sqliteConnection)
